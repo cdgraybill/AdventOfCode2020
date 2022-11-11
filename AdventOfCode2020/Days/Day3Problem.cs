d2a6d880d11746dc429a92379b4d8ca7f08a5813
cdgraybill <cdgraybill@gmail.com>
2022-11-11 14:03:30 -0600

    day 3 part 1

---
+++ AdventOfCode2020/Days/Day3Problem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day3Problem
+    {
+        public int ForestLinePosition { get; set; }
+
+        public List<string> ParseInput()
+        {
+            List<string> input = File.ReadLines(@"C:ProblemInputs\Day3.txt")
+                .ToList();
+
+            return input;
+        }
+
+        public int GetTobogganLocationInForestLine(string forestLine, int currentPosition)
+        {
+            ForestLinePosition = currentPosition + 3;
+
+            if (ForestLinePosition >= forestLine.Length)
+            {
+                var difference = ForestLinePosition - forestLine.Length;
+                ForestLinePosition = difference;
+            }
+
+            return ForestLinePosition;
+        }
+
+        public int CountTrees(List<string> forest)
+        {
+            int numberOfTrees = 0;
+
+            for (int i = 1; i < forest.Count; i++)
+            {
+                var forestLine = forest[i];
+                var newPosition = GetTobogganLocationInForestLine(forestLine, ForestLinePosition);
+
+                if (forestLine[newPosition] == '#')
+                    numberOfTrees++;
+            }
+
+            return numberOfTrees;
+        }
+    }
+}
---
+++ AdventOfCode2020Tests/Day3Tests.cs
@@ -0,0 +1,76 @@
+﻿using AdventOfCode2020.Days;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020Tests
+{
+    internal class Day3Tests
+    {
+        private readonly List<string> testForest = new List<string>
+        {
+          "..##.......",
+          "#...#...#..",
+          ".#....#..#.",
+          "..#.#...#.#",
+          ".#...##..#.",
+          "..#.##.....",
+          ".#.#.#....#",
+          ".#........#",
+          "#.##...#...",
+          "#...##....#",
+          ".#..#...#.#",
+        };
+
+        [Test]
+        public void GetLengthOfInputLine()
+        {
+            var day3 = new Day3Problem();
+            var lineLength = day3.ParseInput()[0].Length;
+
+            Assert.AreEqual(31, lineLength);
+        }
+
+        [Test]
+        public void GetTobogganLocationInForestLine_NoDifference()
+        {
+            var day3 = new Day3Problem();
+            var currentPosition = day3.ForestLinePosition = 0;
+            var position = day3.GetTobogganLocationInForestLine(testForest[0], currentPosition);
+
+            Assert.AreEqual(3, position);
+        }
+
+        [Test]
+        public void GetTobogganLocationInForestLine_Difference()
+        {
+            var day3 = new Day3Problem();
+            var currentPosition = day3.ForestLinePosition = 9;
+            var position = day3.GetTobogganLocationInForestLine(testForest[0], currentPosition);
+
+            Assert.AreEqual(1, position);
+        }
+
+        [Test]
+        public void TestInput()
+        {
+            var day3 = new Day3Problem();
+            var forest = day3.ParseInput();
+            var numberOfTrees = day3.CountTrees(forest);
+
+            Assert.AreEqual(7, numberOfTrees);
+        }
+
+        [Test]
+        public void CountTrees_Success()
+        {
+            var day3 = new Day3Problem();
+            var numberOfTrees = day3.CountTrees(testForest);
+
+            Assert.AreEqual(7, numberOfTrees);
+        }
+    }
+}
