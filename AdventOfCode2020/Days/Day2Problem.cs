d8c91af717263310c96657c577ecd3036cebf832
cdgraybill <cdgraybill@gmail.com>
2022-11-11 00:15:52 -0600

    day 2 solved

--- AdventOfCode2020/Days/Day2Problem.cs
+++ AdventOfCode2020/Days/Day2Problem.cs
@@ -8,27 +8,117 @@ namespace AdventOfCode2020.Days
 {
     public class Day2Problem
     {
+        private readonly string Colon = ": ";
+        private readonly string Hyphen = "-";
+
         public List<string> ParseInput()
         {
             List<string> input = File.ReadLines(@"C:ProblemInputs\Day2.txt")
                 .ToList();
 
-            foreach (var line in input)
-            {
-                switch (line)
-                {
-                    
-                }
-            }
-
             return input;
         }
 
-        public string GetPasswordPolicy(string password)
+        public int CountNumberOfValidPasswordsPartOne()
         {
-            var passwordPolicy = string.Concat(password.TakeWhile((c) => c != ':'));
+            var numberOfValidPasswords = 0;
+            var input = ParseInput();
+
+            foreach (var index in input)
+            {
+                var password = GetPassword(index);
+                var passwordPolicy = GetPasswordPolicy(index);
+                var isValidPassword = CheckForValidPassword(password, passwordPolicy);
+
+                if (isValidPassword)
+                    numberOfValidPasswords++;
+            }
+
+            return numberOfValidPasswords;
+        }
+
+        public int CountNumberOfValidPasswordsPartTwo()
+        {
+            var numberOfValidPasswords = 0;
+            var input = ParseInput();
+
+            foreach (var index in input)
+            {
+                var password = GetPassword(index);
+                var passwordPolicy = GetPasswordPolicy(index);
+                var isValidPassword = CheckForValidPasswordPartTwo(password, passwordPolicy);
+
+                if (isValidPassword)
+                    numberOfValidPasswords++;
+            }
+
+            return numberOfValidPasswords;
+        }
+
+        public string GetPasswordPolicy(string input)
+        {
+            var passwordPolicy = input.Split(Colon, StringSplitOptions.RemoveEmptyEntries)[0];
 
             return passwordPolicy;
         }
+
+        public CharacterRange GetRangeOfAllowedCharacters(string passwordPolicy)
+        {
+            var allowedRange = passwordPolicy.Split(Hyphen, StringSplitOptions.RemoveEmptyEntries);
+
+            var characterRange = new CharacterRange
+            {
+                Min = int.Parse(allowedRange[0]),
+                Max = int.Parse(allowedRange[1].Remove(allowedRange[1].Length - 2))
+            };
+
+            return characterRange;
+        }
+
+        public string GetCharacterToCheck(string passwordPolicy)
+        {
+            var character = passwordPolicy.Substring(passwordPolicy.Length - 1);
+
+            return character;
+        }
+
+        public string GetPassword(string input)
+        {
+            var password = input.Split(Colon, StringSplitOptions.RemoveEmptyEntries)[1];
+
+            return password;
+        }
+
+        public bool CheckForValidPassword(string password, string passwordPolicy)
+        {
+            var numberOfAppearances = 0;
+            var character = GetCharacterToCheck(passwordPolicy);
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i].ToString() == character)
+                    numberOfAppearances++;
+            }
+
+            var characterRange = GetRangeOfAllowedCharacters(passwordPolicy);
+
+            return numberOfAppearances >= characterRange.Min && numberOfAppearances <= characterRange.Max;
+        }
+
+        public bool CheckForValidPasswordPartTwo(string password, string passwordPolicy)
+        {
+            var character = GetCharacterToCheck(passwordPolicy);
+            var characterRange = GetRangeOfAllowedCharacters(passwordPolicy);
+            var positionOne = password[characterRange.Min - 1].ToString();
+            var positionTwo = password[characterRange.Max - 1].ToString();
+
+            return (character == positionOne && character != positionTwo) || (character == positionTwo && character != positionOne);
+        }
+    }
+
+    public class CharacterRange
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
     }
 }
--- AdventOfCode2020Tests/Day2Tests.cs
+++ AdventOfCode2020Tests/Day2Tests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2020Tests
@@ -11,13 +12,73 @@ namespace AdventOfCode2020Tests
     public class Day2Tests
     {
         [Test]
-        public void GetsPasswordPolicy()
+        public void GetPasswordPolicy_Success()
         {
             var day2 = new Day2Problem();
             var input = "1-3 f: asff";
             var passwordPolicy = day2.GetPasswordPolicy(input);
 
             Assert.AreEqual("1-3 f", passwordPolicy);
+        }
+
+        [Test]
+        public void GetPassword_Success()
+        {
+            var day2 = new Day2Problem();
+            var input = "1-3 f: asff";
+            var password = day2.GetPassword(input);
+
+            Assert.AreEqual("asff", password);
+        }
+
+        [Test]
+        public void GetCharacterToCheck_Success()
+        {
+            var day2 = new Day2Problem();
+            var passwordPolicy = "1-3 f";
+            var character = day2.GetCharacterToCheck(passwordPolicy);
+
+            Assert.AreEqual("f", character);
+        }
+
+        [Test]
+        public void GetRangeOfAllowedCharacters_Success()
+        {
+            var day2 = new Day2Problem();
+            var passwordPolicy = "1-3 f";
+            var characterRange = day2.GetRangeOfAllowedCharacters(passwordPolicy);
+
+            Assert.AreEqual(characterRange.Min, 1);
+            Assert.AreEqual(characterRange.Max, 3);
+        }
+
+        [Test]
+        public void CheckForValidPassword_Success()
+        {
+            var day2 = new Day2Problem();
+            var password = "asff";
+            var passwordPolicy = "1-3 f";
+            var isValid = day2.CheckForValidPassword(password, passwordPolicy);
+
+            Assert.True(isValid);
+        }
+
+        [Test]
+        public void CountNumberOfValidPasswords_PartOne()
+        {
+            var day2 = new Day2Problem();
+            var numberOfValidPasswords = day2.CountNumberOfValidPasswordsPartOne();
+
+            Assert.AreEqual(2, numberOfValidPasswords);
+        }
+
+        [Test]
+        public void CountNumberOfValidPasswords_PartTwo()
+        {
+            var day2 = new Day2Problem();
+            var numberOfValidPasswords = day2.CountNumberOfValidPasswordsPartTwo();
+
+            Assert.AreEqual(2, numberOfValidPasswords);
         }
     }
 }
