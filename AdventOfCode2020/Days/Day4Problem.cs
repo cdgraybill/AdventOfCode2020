6d301752ddf678a208a6de072dcfa512544bcb28
cdgraybill <cdgraybill@gmail.com>
2022-11-11 20:18:29 -0600

    solving day4 part 1

---
+++ AdventOfCode2020/Days/Day4Problem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days
+{
+    public class Day4Problem
+    {
+        public Dictionary<string, string> ParseInputIntoPassport(List<string> input)
+        {
+            Dictionary<string, string> output = new Dictionary<string, string>();
+            var sb = new StringBuilder();
+
+            foreach (var inputItem in input)
+            {
+                if (!string.IsNullOrEmpty(inputItem))
+                    sb.Append(inputItem);
+                else
+                    break;
+            }
+
+            var passportFields = sb.ToString().Split(" ");
+            foreach (var passportField in passportFields)
+            {
+
+            }
+
+            return null;
+        }
+
+        public PassportField SplitFieldIntoKeyValuePair(string fieldString)
+        {
+            var splitString = fieldString.Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+            var passportField = new PassportField()
+            {
+                Key = splitString[0],
+                Value = splitString[1]
+            };
+
+            return passportField;
+        }
+
+    }
+
+    public class PassportField
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ AdventOfCode2020Tests/Day4Tests.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2020.Days;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020Tests
+{
+    public class Day4Tests
+    {
+        private readonly List<string> TestInput = new List<string>
+        {
+            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+            "byr:1937 iyr:2017 cid:147 hgt:183cm",
+            "",
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+            "hcl:#cfa07d byr:1929",
+            "",
+            "hcl:#ae17e1 iyr:2013",
+            "eyr:2024",
+            "ecl:brn pid:760753108 byr:1931",
+            "hgt:179cm",
+            "",
+            "hcl:#cfa07d eyr:2025 pid:166559648",
+            "iyr:2011 ecl:brn hgt:59in"
+        };
+
+        public List<string> problemInput = File.ReadLines(@"C:ProblemInputs\Day4.txt").ToList();
+
+        [Test]
+        public void ParseInputIntoPassport()
+        {
+            var day4 = new Day4Problem();
+            var passports = day4.ParseInputIntoPassport(TestInput);
+        }
+
+        [Test]
+        public void SplitFieldIntoKeyValuePair_Success()
+        {
+            var day4 = new Day4Problem();
+            var fieldString = "ecl:gry";
+            var passportField = day4.SplitFieldIntoKeyValuePair(fieldString);
+
+            Assert.IsNotNull(passportField);
+            Assert.AreEqual("ecl", passportField.Key);
+            Assert.AreEqual("gry", passportField.Value);
+        }
+    }
+}
