6d6cee4d47b0ee41c316c61a364c372e49d88c6d
cdgraybill <cdgraybill@gmail.com>
2022-11-12 02:34:37 -0600

    day 4 part 1 solved

--- AdventOfCode2020/Days/Day4Problem.cs
+++ AdventOfCode2020/Days/Day4Problem.cs
@@ -10,16 +10,7 @@ namespace AdventOfCode2020.Days
 {
     public class Day4Problem
     {
-        public Dictionary<string, string> ParseInputIntoPassport(List<string> input)
-        {
-            Dictionary<string, string> passport = new Dictionary<string, string>();
-            var sb = new StringBuilder();
-
-            GetPassportFields(input, sb);
-            CreatePassportDictionary(passport, sb);
-
-            return passport;
-        }
+        private readonly string[] MandatoryKeys = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
 
         public PassportField SplitFieldIntoKeyValuePair(string fieldString)
         {
@@ -34,25 +25,58 @@ namespace AdventOfCode2020.Days
             return passportField;
         }
 
-        private void CreatePassportDictionary(Dictionary<string, string> passport, StringBuilder sb)
+        private void AddFieldsToPassport(Dictionary<string, string> passport, StringBuilder sb)
         {
             var passportFields = sb.ToString().Split(" ");
-            foreach (var passportField in passportFields)
+            var adjustedFields = passportFields.SkipLast(1).ToArray();
+            foreach (var passportField in adjustedFields)
             {
                 var keyValuePair = SplitFieldIntoKeyValuePair(passportField);
                 passport[keyValuePair.Key] = keyValuePair.Value;
             }
         }
 
-        private static void GetPassportFields(List<string> input, StringBuilder sb)
+        public bool IsValidPassport(Dictionary<string, string> passport)
         {
-            foreach (var inputItem in input)
+            var isValid = true;
+
+            foreach (var mandatoryKey in MandatoryKeys)
             {
-                if (!string.IsNullOrEmpty(inputItem))
-                    sb.Append(inputItem);
-                else
+                if (!passport.Keys.Contains(mandatoryKey))
+                {
+                    isValid = false;
                     break;
+                }
             }
+
+            return isValid;
+        }
+
+        public int GetNumberOfValidPasswords(List<string> problemInput)
+        {
+            var numberOfValidPasswords = 0;
+            var passports = new List<Dictionary<string, string>>();
+            var sb = new StringBuilder();
+
+            for (int i = 1; i <= problemInput.Count; i++)
+            {
+                if (!string.IsNullOrEmpty(problemInput[i - 1]))
+                    sb.Append(problemInput[i - 1] + " ");
+                
+                if(string.IsNullOrEmpty(problemInput[i - 1]) || i == problemInput.Count)
+                {
+                    var passport = new Dictionary<string, string>();
+                    AddFieldsToPassport(passport, sb);
+                    passports.Add(passport);
+
+                    if (IsValidPassport(passport))
+                        numberOfValidPasswords++;
+
+                    sb.Clear();
+                }
+            }
+
+            return numberOfValidPasswords;
         }
     }
 
--- AdventOfCode2020Tests/Day4Tests.cs
+++ AdventOfCode2020Tests/Day4Tests.cs
@@ -12,35 +12,7 @@ namespace AdventOfCode2020Tests
 {
     public class Day4Tests
     {
-        private readonly List<string> TestInput = new List<string>
-        {
-            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
-            "byr:1937 iyr:2017 cid:147 hgt:183cm",
-            "",
-            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
-            "hcl:#cfa07d byr:1929",
-            "",
-            "hcl:#ae17e1 iyr:2013",
-            "eyr:2024",
-            "ecl:brn pid:760753108 byr:1931",
-            "hgt:179cm",
-            "",
-            "hcl:#cfa07d eyr:2025 pid:166559648",
-            "iyr:2011 ecl:brn hgt:59in"
-        };
-
-        public List<string> problemInput = File.ReadLines(@"C:ProblemInputs\Day4.txt").ToList();
-
-        [Test]
-        public void ParseInputIntoPassport()
-        {
-            var day4 = new Day4Problem();
-            var passport = day4.ParseInputIntoPassport(TestInput);
-
-            Assert.IsInstanceOf<Dictionary<string, string>>(passport);
-            Assert.AreEqual("ecl", passport.Keys.First());
-            Assert.AreEqual("gry", passport.Values.First());
-        }
+        private readonly List<string> ProblemInput = File.ReadLines(@"C:ProblemInputs\Day4.txt").ToList();
 
         [Test]
         public void SplitFieldIntoKeyValuePair_Success()
@@ -52,6 +24,15 @@ namespace AdventOfCode2020Tests
             Assert.IsNotNull(passportField);
             Assert.AreEqual("ecl", passportField.Key);
             Assert.AreEqual("gry", passportField.Value);
+        }
+
+        [Test]
+        public void GetNumberOfValidPassports()
+        {
+            var day4 = new Day4Problem();
+            var numberOfValidPasswords = day4.GetNumberOfValidPasswords(ProblemInput);
+
+            Assert.AreEqual(2, numberOfValidPasswords);
         }
     }
 }
