9e4e2259ab2c81e7cd912d8ae628fae4197b4d93
cdgraybill <cdgraybill@gmail.com>
2022-11-13 02:46:43 -0600

    starting day5 part1

--- AdventOfCode2020/Days/Day5Problem.cs
+++ AdventOfCode2020/Days/Day5Problem.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2020.Days
 {
-    internal class Day5Problem
+    public class Day5Problem
     {
+        private readonly int[] _numberOfRows = new int[128];
+        private readonly int[] _numberOfColumns = new int[9];
+
+        public SeatingInstructions SplitSeatingInstructions(string rawInstrutions)
+        {
+            var seatingInstructions = new SeatingInstructions()
+            {
+                Row = rawInstrutions[..7],
+                Seat = rawInstrutions.Substring(7, 3)
+            };
+
+            return seatingInstructions;
+        }
+
+        public int GetSeatingLocation(char directionOne, char directionTwo, string seatingInstruction)
+        {
+            int seatingLocation = 0;
+
+            foreach (var direction in seatingInstruction)
+            {
+                switch (direction)
+                {
+                    case var _ when direction.Equals(directionOne):
+
+                        break;
+                    case var _ when direction.Equals(directionTwo):
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return 0;
+        }
+    }
+
+    public class SeatingInstructions
+    {
+        public string Row { get; set; }
+        public string Seat { get; set; }
     }
 }
--- AdventOfCode2020Tests/Day5Tests.cs
+++ AdventOfCode2020Tests/Day5Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdventOfCode2020.Days;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2020Tests
 {
-    internal class Day5Tests
+    public class Day5Tests
     {
+        private readonly string[] TestInput =
+        {
+            "BFFFBBFRRR",
+            "FFFBBBFRRR",
+            "BBFFBBFRLL"
+        };
+
+        [Test]
+        public void SplitSeatingInstructions()
+        {
+            var day5 = new Day5Problem();
+            var seatingInstructions = day5.SplitSeatingInstructions(TestInput[0]);
+
+            Assert.AreEqual(7, seatingInstructions.Row.Length);
+            Assert.AreEqual(3, seatingInstructions.Seat.Length);
+            Assert.AreEqual("BFFFBBF", seatingInstructions.Row);
+            Assert.AreEqual("RRR", seatingInstructions.Seat);
+        }
+
     }
 }
